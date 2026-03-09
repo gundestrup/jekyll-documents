eccc066d9a556f55bf63cad3c682ea9433d32524
Svend Gundestrup <30799+gundestrup@users.noreply.github.com>
2026-03-09 10:34:01 +0100

    Create json_index_generator.rb

---
+++ lib/jekyll/documents/json_index_generator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "json"
+require_relative "utils"
+
+module Jekyll
+  module Documents
+    class JsonIndexGenerator < ::Jekyll::Generator
+      safe true
+      priority :low
+
+      def generate(site)
+        cfg = Configuration.read(site)
+        return unless cfg["json_index"]
+
+        docs = site.collections["documents"]&.docs || []
+        return if docs.empty?
+
+        items = docs.map do |d|
+          {
+            "url"      => d.url,
+            "title"    => d.data["title"],
+            "category" => d.data["category"],
+            "date"     => (d.data["date"] || Time.at(0)).strftime("%Y-%m-%d"),
+            "slug"     => d.data["slug"]
+          }
+        end
+
+        json = JSON.pretty_generate(items)
+        path = cfg["json_index_path"] || "/documents.json"
+
+        site.static_files << TextStaticFile.new(site, site.source, path, json)
+      end
+    end
+  end
+end
