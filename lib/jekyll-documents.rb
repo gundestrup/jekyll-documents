55ee914add3a5690e37291e24e90e7ba9d099777
Svend Gundestrup <30799+gundestrup@users.noreply.github.com>
2026-03-09 10:31:25 +0100

    Create jekyll-documents.rb

---
+++ lib/jekyll-documents.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "jekyll"
+require_relative "jekyll/documents/version"
+require_relative "jekyll/documents/configuration"
+require_relative "jekyll/documents/utils"
+require_relative "jekyll/documents/filters"
+require_relative "jekyll/documents/generator"
+require_relative "jekyll/documents/json_index_generator"
+
+# Liquid tag(s)
+require_relative "jekyll/documents/tags/latest_documents"
+
+module Jekyll
+  module Documents
+    # Namespace module for the plugin.
+  end
+end
